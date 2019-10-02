ead038e2c246b889f54e06f18f261a85c90751d8
Jono Green <jono.green7@gmail.com>
2019-10-02 20:07:10 +1000

    Add cross-spawn dep + test input command

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,23 @@
 const qoa = require('qoa');
-
-const {log} = console;
+const spawn = require('cross-spawn');
 
 const ps = [
   {
     type: 'input',
-    query: 'Name your command:',
+    query: 'Enter a command:',
     handle: 'name'
   },
 ];
 
-const cli = () => {
-	qoa.prompt(ps).then(log);
+const cli = async () => {
+  const responses = await qoa.prompt(ps);
+
+  const fullCommand = responses.name; // 'yarn -D add @types/testing';
+  const [command, ...args] = fullCommand.split(' ');
+  
+  const p = spawn(command, args, { stdio: 'inherit' });
+  p.on('error', i => console.error('cr failed for:', i.path)) // Report general failure
+  // c.on('close', i => console.log('Stopping cr process')) // TODO: decide if needed
 };
 
 module.exports = cli;
